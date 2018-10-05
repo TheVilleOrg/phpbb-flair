154a12a765dc0e8c804435e0acc6ae0adac0db2b
Steve Guidetti <stevotvr@gmail.com>
2018-10-05 11:29:15 -0700

    Fix language key conflict

--- language/en/info_ucp_main.php
+++ language/en/info_ucp_main.php
@@ -50,5 +50,5 @@ $lang = array_merge($lang, array(
 	'UCP_FLAIR_UNFAV'			=> 'Remove %s from favorites',
 	'UCP_FLAIR_REMOVE_CONFIRM'	=> 'Are you sure you wish to remove this item?',
 
-	'UCP_FLAIR_ADD'	=> 'Add to profile',
+	'UCP_FLAIR_ADD_BUTTON'	=> 'Add to profile',
 ));
